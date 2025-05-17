691b5399b44cbcebdb1886b8cedea2ad6412e469
Jeffrey-Nieto <jenieto@student.fullsail.edu>
2025-05-17 14:51:54 -0500

    Update MiningNode.cs

--- WtGB_Project/Assets/Scripts/MiningNode.cs
+++ WtGB_Project/Assets/Scripts/MiningNode.cs
@@ -5,7 +5,8 @@ public class MiningNode : MonoBehaviour
     [Header("Ore Data")]
     [SerializeField] private int hitsToBreak = 3;
     [SerializeField] private GameObject oreDropPrefab;
-    [SerializeField] private OreItem oreData;
+    // Gave me an error because OreItem script was not pushed so it could not find the reference if you could please push that with next commit
+    // [SerializeField] private OreItem oreData;
     [SerializeField] private Vector3 dropOffset = Vector3.up;
 
     private int currentHits = 0;
@@ -26,10 +27,11 @@ public class MiningNode : MonoBehaviour
 
     private void BreakNode()
     {
-        if(oreDropPrefab != null && oreData != null)
+        if(oreDropPrefab != null/* && oreData != null*/)
         {
             GameObject drop = Instantiate(oreDropPrefab, transform.position + dropOffset, Quaternion.identity);
-            drop.GetComponent<OrePickup>().oreData = oreData;
+            // Reason why it's commented at top of script
+            //drop.GetComponent<OrePickup>().oreData = oreData;
         }
         Destroy(gameObject);
     }
